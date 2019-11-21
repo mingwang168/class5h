1512c15add079d594d53462a13399bf92b2d7a07
mingwang168 <mingwang0904@gmail.com>
2019-11-21 15:47:40 -0800

    dfas

--- class5h/Program.cs
+++ class5h/Program.cs
@@ -6,26 +6,31 @@ namespace class5h
     {
         public delegate void MyDelegate(string output);// Declare delegate.
         public event MyDelegate BeginOutput;   // Declare event.
-
+        public event MyDelegate EndOutput;   // Declare event.
         public void Display(string output)
-        {
-            NotifySubscriber();
-            Console.WriteLine(output);                 // Show output.
-        }
-        private void NotifySubscriber()
         {
             if (BeginOutput != null)
             {
                 BeginOutput("Starting output event."); // Raise event.
             }
+            Console.WriteLine(output);
+            if (EndOutput != null)
+            {
+                EndOutput("Ending output event."); // Raise event.
+            }// Show output.
         }
+
     }
     public class Program
     {
         // Declare a callback method to receive event notifications.
         public static void OnBeginOutput(string output)
         {
-            Console.WriteLine("Inside callback: " + output);
+            Console.WriteLine("Inside begin callback: " + output);
+        }
+        public static void OnEndOutput(string output)
+        {
+            Console.WriteLine("Inside end callback: " + output);
         }
         public static void Main()
         {
@@ -33,14 +38,15 @@ namespace class5h
 
             // Subscribe to event and reference OnBeginOutput() callback.
             publisher.BeginOutput += OnBeginOutput;
+            publisher.EndOutput += OnEndOutput;
 
-            publisher.Display("Hello!  I am a subscriber.");
+            publisher.Display("Hello!  I am a subscriber to both events.");
 
             // De-reference OnBeginOutput() callback method.
             publisher.BeginOutput -= OnBeginOutput;
 
             // Output text when not subscribed to the event.
-            publisher.Display("Hello!  I am not a subscriber.");
+            publisher.Display("Hello!  I am a subscriber only to the end event.");
             Console.ReadLine();
         }
     }
